6b8d3239de3a5ced1c35e2fec76e489257089aca
Alden <aldenefendic0@gmail.com>
2022-12-06 18:56:33 +0100

    Advanced Java prva zadaca

---
+++ src/main/java/org/example/JavaAdvancedTest1/ArrayDemo.java
@@ -0,0 +1,23 @@
+package org.example.JavaAdvancedTest1;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        DynamicArray dynamicArray=new DynamicArray();
+        dynamicArray.add(23);
+        dynamicArray.add(16);
+        dynamicArray.add(17);
+        dynamicArray.add(22);
+        dynamicArray.add(12);
+        dynamicArray.add(17);
+        dynamicArray.add(33);
+        for (int i:dynamicArray){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        System.out.println("Novi niz: ");
+        dynamicArray.remove(17);
+        for (int i:dynamicArray){
+            System.out.print(i+" ");
+        }
+    }
+}
---
+++ src/main/java/org/example/JavaAdvancedTest1/DynamicArray.java
@@ -0,0 +1,45 @@
+package org.example.JavaAdvancedTest1;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class DynamicArray implements Iterable<Integer>{
+    private int capacity = 0;
+    private int[] numbers=new int[capacity];
+
+    public void add(int number){
+        capacity++;
+        numbers = Arrays.copyOf(numbers,capacity);
+        numbers[capacity-1]=number;
+    }
+    public void remove(int number){
+        for(int i=0; i<capacity; i++){
+            if (numbers[i]==number){
+                capacity--;
+                for (int j=i; j<capacity; j++) {
+                    numbers[j] = numbers[j + 1];
+                }
+            }
+            numbers = Arrays.copyOf(numbers,capacity);
+        }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iteracija();
+    }
+    private class Iteracija implements Iterator<Integer>{
+        private int indexNumber=0;
+        @Override
+        public boolean hasNext() {
+            return (indexNumber<capacity);
+        }
+
+        @Override
+        public Integer next() {
+            int number=numbers[indexNumber];
+            indexNumber++;
+            return number;
+        }
+    }
+}
